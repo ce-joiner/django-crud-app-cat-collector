15c9fd2798ed789038b97a55ad73ef281aea98a3
Casey Joiner <caseyejoiner@gmail.com>
2025-05-19 14:48:15 -0500

    Add environment variable support and update SECRET_KEY configuration

--- catcollector/settings.py
+++ catcollector/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q81yj@k5&9_-_*2lnz6&0^ny^l!+kqxuy9o0&1%#gu99chkuwh'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
