a9ccc8c40e3e32d32d92e320b712cfa58941b35c
Casey Joiner <caseyejoiner@gmail.com>
2025-05-20 19:00:24 -0500

    Implement cat management features: update database settings, create Cat model, add views and templates for cat listing, and style with CSS.

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     # Routes will be added here
     path('', views.home, name='home'), # Home page route
     path('about/', views.about, name='about'), # About page route
+    path('cats/', views.cat_index, name='cat_index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 # Define the home view function
 def home(request):
-    return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+    return render(request, 'home.html') # Render the home.html template
 
 def about(request):
     return render(request, 'about.html') # Render the about.html template
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+# Define the cat_index view function
+def cat_index(request):
+    return render(request, 'cats/index.html', {'cats': cats}) # Render the index.html template with the list of cats
